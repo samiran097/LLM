5b5f57e78592827fa933644dcfd9c538435a5c1d
samiran097 <samiranadak097@gmail.com>
2023-12-28 23:32:11 +0530

    files updated

--- setup.py
+++ setup.py
@@ -1,2 +1,13 @@
 ## TO INSTALL LOCAL PACKAGES IN MY VIRTUAL ENVIRONMENT
 
+from setuptools import find_packages,setup
+
+setup(
+    name = 'mcqgenerator',
+    version = '0.0.1',
+    author = 'Samiran Adak',
+    author_email= 'samiranadak097@gmail.com',
+    install_requires = ["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages = find_packages() # It finds the local package from .init file, it will consoder that folder as a package
+
+)
